8d556e8f75ea2e1a825f0a4c2d461ad8e8fc8531
Gabriele Busnelli <busnelli.gabriele.99@gmail.com>
2023-06-21 15:31:58 +0200

    created utenteRepository created entity Utente

--- src/main/java/it/generationitaly/app/repository/UtenteRepository.java
+++ src/main/java/it/generationitaly/app/repository/UtenteRepository.java
@@ -1,5 +1,12 @@
 package it.generationitaly.app.repository;
 
+import java.util.List;
+
 public interface UtenteRepository {
 
+	void save(Utente utente);
+	void update(utente utente);
+	void delete(Utente utente);
+	Utente findById(int id);
+	List<Utente> findAll();
 }
