4406ce915d2e0f2d7b9473d7be052ee6f8ab77ad
Gabriele Busnelli <busnelli.gabriele.99@gmail.com>
2023-06-21 15:24:57 +0200

    created package impl e classe PersonaRepositoryImpl created method save into PersonaRepositoryImpl

---
+++ src/main/java/it/generationitaly/app/repository/impl/PersonaRepositoryImpl.java
@@ -0,0 +1,13 @@
+package it.generationitaly.app.repository.impl;
+
+import it.generationitaly.app.entity.Persona;
+import it.generationitaly.app.repository.PersonaRepository;
+
+public class PersonaRepositoryImpl implements PersonaRepository{
+
+	public void save(Persona persona) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
