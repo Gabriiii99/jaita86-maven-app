1833e404826d03844f90bf935eafeec5b73504a6
Gabri <Gabri@Chuck.homenet.telecomitalia.it>
2023-06-21 15:06:57 +0200

    added property id, codice fiscale, nome, cognome created personaRepository.java

--- src/main/java/it/generationitaly/app/entity/Persona.java
+++ src/main/java/it/generationitaly/app/entity/Persona.java
@@ -2,4 +2,55 @@ package it.generationitaly.app.entity;
 
 public class Persona {
 
+	private int id;
+	private String codiceFiscale;
+	private String nome;
+	private String cognome;
+
+	public Persona(int id, String codiceFiscale, String nome, String cognome) {
+		super();
+		this.id = id;
+		this.codiceFiscale = codiceFiscale;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", codiceFiscale=" + codiceFiscale + ", nome=" + nome + ", cognome=" + cognome
+				+ "]";
+	}
+
 }
---
+++ src/main/java/it/generationitaly/app/repository/PersonaRepository.java
@@ -0,0 +1,9 @@
+package it.generationitaly.app.repository;
+
+import it.generationitaly.app.entity.Persona;
+
+public interface PersonaRepository {
+
+	void save(Persona persona);
+	
+}
